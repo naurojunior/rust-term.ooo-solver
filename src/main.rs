fc1bba317b682210e99e1c252751e0414a34b19e
Nauro <nauro@outlook.com>
2022-05-09 14:27:15 -0300

    Rename force_generate_dictionary to is_force_generate_dictionary

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn should_generate_dictionary(force_generate_dictionary: bool, output_file_exist
     force_generate_dictionary || !output_file_exists
 }
 
-fn force_generate_dictionary(args: Vec<String>) -> bool{
+fn is_force_generate_dictionary(args: Vec<String>) -> bool{
     
     args.iter()
         .filter(|&argument| { argument == "--force-generate-dictionary" || argument == "-f" })
@@ -23,7 +23,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let output_file_name = "words-5-letters.txt";
 
-    if should_generate_dictionary(force_generate_dictionary(args), output_file_exists(output_file_name)) {
+    if should_generate_dictionary(is_force_generate_dictionary(args), output_file_exists(output_file_name)) {
         println!("{}", "Dictionary generated");
         dictionary::generate("words.txt", output_file_name)
     }else{
